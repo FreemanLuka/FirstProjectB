8a74990361c4b552d758ef05918678781b4268bb
FreemanLuka <freemanluka@gmail.com>
2025-09-16 02:16:20 +0100

    Added header

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <nav className='header-nav'>
+        <li>About me</li>
+        <li>Skills</li>
+        <li>Portfolio</li>
+        <button className='header-nav-btn'>CONTACT ME</button>
+      </nav>
+  )
+}
+
+export default Header
